334de4bdf98ff6b79f761443338a9f61c0de7d21
kopika <taniafci81@gmail.com>
2019-05-20 09:27:37 +0600

    function Base rest_framework

--- rest_framework_crud/function_crud/urls.py
+++ rest_framework_crud/function_crud/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import  dataView
+from .views import dataView,dataDetailsView
 urlpatterns=[
-    path('all_data',dataView)
+    path('all_data',dataView),
+    path('all_data/<int:id>',dataDetailsView)
 ]
--- rest_framework_crud/function_crud/views.py
+++ rest_framework_crud/function_crud/views.py
@@ -3,6 +3,7 @@ from django.http import  HttpResponseRedirect,HttpRequest
 from .models import FunctionCrudModel
 from .serializers import FunctionCrudSerializer
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
@@ -20,5 +21,26 @@ def dataView(request):
             serialized.save()
             return JsonResponse(serialized.data,status=201)
         return JsonResponse(serialized.errors,status=400)
+
+@csrf_exempt
+def dataDetailsView(request,id):
+    try:
+        instance=get_object_or_404(FunctionCrudModel,id=id)
+    except:
+        return JsonResponse({'error':'Object Not found'},status=403)
+    if request.method == 'GET':
+        serializer=FunctionCrudSerializer(instance)
+        return JsonResponse(serializer.data)
+    elif request.method =='PUT':
+        parser=JSONParser()
+        data=parser.parse(request)
+        update_serializer=FunctionCrudSerializer(instance,data=data)
+        if update_serializer.is_valid():
+            update_serializer.save()
+            return JsonResponse(update_serializer.data,status=200)
+        return JsonResponse(update_serializer.error,status=400)
+    elif request.method == 'DELETE':
+        instance.delete()
+        return HttpRequest(status=204)
 
 
